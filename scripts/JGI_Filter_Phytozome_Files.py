e0b8aca04ca4b5b5c68e3b6b64716e96576dceec
Sam Seaver <samseaver@gmail.com>
2025-04-01 23:59:40 -0500

    Rewrote script for filtering files better, and filtered files for several new species

--- scripts/JGI_Filter_Phytozome_Files.py
+++ scripts/JGI_Filter_Phytozome_Files.py
@@ -21,25 +21,9 @@ with open('Input_Files/Base_File_Version.txt') as bvfh:
         tmp_lst = line.split('\t')
         base_file_versions[tmp_lst[0]]={'assembly':tmp_lst[1],'annotation':tmp_lst[2]}
 
-base_species=dict()
-with open('Input_Files/Base_Species.txt') as bvfh:
-    for line in bvfh.readlines():
-        line = line.strip('\r\n')
-        tmp_lst = line.split('\t')
-        base_species[tmp_lst[0]]=tmp_lst[1]
-
 file_types = ['gff','fasta','text']
-accepted_files=list()
+accepted_files=dict()
 for ff in organism_files:
-    if(ff['type'] not in file_types):
-        continue
-
-    if('data_release_policy' not in ff or ff['data_release_policy'] != 'unrestricted'):
-        continue
-
-    if(ff['Gspecies'] in base_species):
-        ff['Gspecies'] = base_species[ff['Gspecies']]
-
     species_version = "_".join([ff['Gspecies'],ff['annotation_version']])
 
     annotation = ff['annotation_version']
@@ -48,35 +32,95 @@ for ff in organism_files:
        annotation=base_file_versions[species_version]['annotation']
        assembly=base_file_versions[species_version]['assembly']
 
-    accepted=False
-    base_gm_file = "_".join([ff['Gspecies'],str(ff['proteome_id']),annotation])
-    base_as_file = "_".join([ff['Gspecies'],str(ff['proteome_id']),assembly])
-    if(ff['type'] == 'gff'):
-        if(ff['file'] == ".".join([base_gm_file,"gene","gff3","gz"])):
-            accepted=True
+#    if(ff['Gspecies'].startswith("Bdistachyon") and ff['Gspecies'] != "Bdistachyon"):
+#        Bdi_accession = ff['Gspecies'][len('Bdistachyon'):]
+#        annotation = annotation.split(".")
+#        annotation.insert(1,Bdi_accession)
+#        if(annotation[-1] != "1"):
+#            annotation.append("1")
+#        annotation = ".".join(annotation)
 
-    if(ff['type'] == 'fasta'):
-        if(ff['file'] == ".".join([base_gm_file,"protein","fa","gz"])):
-            accepted=True
+    base_gm_file = "_".join([ff['Gspecies'],str(ff['proteome_id']),annotation]).lower()
+    base_as_file = "_".join([ff['Gspecies'],str(ff['proteome_id']),assembly]).lower()
 
-        if(ff['file'] == ".".join([base_as_file,"fa","gz"])):
-            accepted=True
+    # strip key characters
+    strip_table = str.maketrans({"_":"","-":"",",":"","'":""})
+    stripped_gm_file = base_gm_file.translate(strip_table)
+    stripped_as_file = base_as_file.translate(strip_table)
 
-        if(species_version in missing_assemblies_dict and ff['file'] == missing_assemblies_dict[species_version]):
-            accepted=True
+    base_key = "_".join([ff['Gspecies'],str(ff['proteome_id'])])
+    if(base_key not in accepted_files):
+        accepted_files[base_key]=list()
+        
+    # Gene Model
+    if(ff['type'] == 'gff' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"gene","gff3","gz"])):
+        accepted_files[base_key].append(ff)
 
-    if(ff['type'] == 'text'):
-        if(ff['file'] == ".".join([base_gm_file,"annotation_info","txt"])):
-            accepted=True
+    # Gunzipped Gene Model
+    if(ff['type'] == 'gff' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"gene","gff3"])):
+        accepted_files[base_key].append(ff)
 
-        if(ff['file'] == ".".join([base_gm_file,"synonym","txt"])):
-            accepted=True
+    # Assembly
+    if(ff['type'] == 'fasta' and ff['file'].translate(strip_table).lower() == ".".join([stripped_as_file,"fa","gz"])):
+        accepted_files[base_key].append(ff)
 
-        if(ff['file'] == ".".join([base_gm_file,"defline","txt"])):
-            accepted=True
+    # Protein
+    if(ff['type'] == 'fasta' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"protein","fa","gz"])):
+        accepted_files[base_key].append(ff)
 
-    if(accepted is True):
-        accepted_files.append(ff)
+    # Annotation
+    if(ff['type'] == 'text' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"annotationinfo","txt"])):
+        accepted_files[base_key].append(ff)
 
+    # Synonyms
+    if(ff['type'] == 'text' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"synonym","txt"])):
+        accepted_files[base_key].append(ff)
+
+    # Defline
+    if(ff['type'] == 'text' and ff['file'].translate(strip_table).lower() == ".".join([stripped_gm_file,"defline","txt"])):
+        accepted_files[base_key].append(ff)
+
+    if(species_version in missing_assemblies_dict and ff['file'] == missing_assemblies_dict[species_version]):
+        accepted_files[base_key].append(ff)
+
+filtered_files = list()
+for base_key in accepted_files:
+    has_gff=False
+    has_assembly=False
+
+    if(base_key.startswith("Bdistachyon") and "Bdistachyon_" not in base_key):
+        bdist_pangenome = True
+        continue
+
+    for ff in accepted_files[base_key]:
+        if(ff['type'] =='gff'):
+            has_gff=True
+
+        if(ff['type'] == 'fasta' and 'protein' not in ff['file']):
+            has_assembly=True
+
+    if(not has_assembly or not has_gff):
+
+        print(base_key,has_assembly,has_gff)
+        for off in organism_files:
+            if(base_key == "_".join([off['Gspecies'],str(off['proteome_id'])])):
+                print("\t",off['type'],off['file'],off['assembly_version'],off['annotation_version'])
+                if('data_release_policy' in off):
+                    print("\t\t",off['data_release_policy'])
+        continue
+
+    drp=list()
+    for ff in accepted_files[base_key]:
+        if(ff['type'] =='gff' or (ff['type'] == 'fasta' and 'protein' not in ff['file'])):
+            if('data_release_policy' not in ff):
+                drp.append(None)
+            else:
+                drp.append(ff['data_release_policy'])                
+    
+    if(len(set(drp)) == 1 and drp[0] == 'unrestricted'):
+        for ff in accepted_files[base_key]:
+            filtered_files.append(ff)
+
+filtered_files = sorted(filtered_files, key = lambda d: [str(d[key]) for key in ("proteome_id","jamo_id")])
 with open(os.path.join('..',db_source,'JGI_Data_Portal_Accepted_Files.json'),'w') as jdpfh:
-    jdpfh.write(json.dumps(accepted_files,indent=2))
+    jdpfh.write(json.dumps(filtered_files,sort_keys=True,indent=2))
